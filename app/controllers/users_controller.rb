37a11050371573606d96f7bd4012f1ef2609bc28
Gor <7495500@gmail.com>
2023-08-22 00:19:28 +0300

    Search few items with ransack try

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,28 +2,24 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
 
   def index
-    @search_query = User.ransack(params[:search_query])
+    @search_query = User.ransack(params[:search_query].try(:merge, m: 'or'))
     @users = @search_query.result
   end
 
-  def show
-  end
+  def show; end
+  def edit; end
 
   def new
     @user = User.new
   end
 
-  def edit
-  end
-
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
+        format.html { redirect_to user_url(@user) }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
       end
     end
   end
@@ -31,18 +27,17 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
+        format.html { redirect_to user_url(@user) }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
       end
     end
   end
 
   def destroy
     @user.destroy
-
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html { redirect_to users_url }
     end
   end
 
